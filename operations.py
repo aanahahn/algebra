23d77c8e3c379401ad41d2b069af79f4ca1fe754
Aana Hahn <aana.kim@gmail.com>
2018-11-15 11:20:20 -0500

    adding operations file

---
+++ operations.py
@@ -0,0 +1,12 @@
+def sum (num1,num2):
+	return num1 + num2 
+"""
+This function returns the sum of 2 numbers
+
+"""
+
+def product (num1,num2):
+	return num1 * num2 
+"""
+This function returns the product of 2 numbers
+"""
